a9b650f62a31fbffe2744db88af9a8a3e596554d
mucahityildiz1 <mucahityyildiz@gmail.com>
2024-10-16 17:38:03 +0300

    first commit

---
+++ assets/js/number.js
@@ -0,0 +1,8 @@
+// Kullanıcıya 1-20 arası bir sayı tahmin etmesini isteyelim.
+// Cevap hakkı 5 olacak.
+// Her bir yanlış cevapta cevap hakkından -1 azalacak.
+// Eğer tahmin ettiği sayı random üretilen sayıdan büyükse 
+// "Daha küçük bir sayı deneyin." , tahmin ettiği sayı random üretilen 
+// sayıdan küçükse "Daha büyük bir sayı deneyin." diye console'da mesaj çıkartalım.
+// Doğru bildiğinde console'da tebrikler yazdıralım.
+
